83959c931d3a855de616f3c4e7e2814a25e373cc
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-10 19:24:17 -0400

    Initial Commit

---
+++ TimeWizardVP/TimeWizardVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeWizardVPApp.swift
+//  TimeWizardVP
+//
+//  Created by Ahsan Qureshi on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeWizardVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
