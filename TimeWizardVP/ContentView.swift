0f6f968e9934c20c3b9465ff84cb04e54b04ff65
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-10 19:47:21 -0400

    VP version of time wizard

--- TimeWizardVP/ContentView.swift
+++ TimeWizardVP/ContentView.swift
@@ -10,14 +10,58 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+    @State private var inputUnit = "Seconds"
+    @State private var outputUnit = "Seconds"
+    @State private var inputTime = 0.0
+    
+    private var timeUnits = ["Seconds", "Minutes", "Hours", "Days"]
+    private var timeConversion = [60, 60, 24, 1]
+    private var outputTime: Double {
+        var inputIndex = timeUnits.firstIndex(of: inputUnit) ?? 0
+        let outputIndex = timeUnits.firstIndex(of: outputUnit) ?? 0
+        var outputValue = inputTime
+        while(inputIndex != outputIndex) {
+            if (inputIndex > outputIndex) {
+                inputIndex -= 1
+                outputValue *= Double(timeConversion[inputIndex])
+            } else {
+                outputValue /= Double(timeConversion[inputIndex])
+                inputIndex += 1
+            }
         }
-        .padding()
+        return outputValue
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Input Unit") {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Output Unit") {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Input time in (\(inputUnit))") {
+                    TextField("Input Time", value: $inputTime, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                Section("Output time in (\(outputUnit))") {
+                    Text(outputTime, format: .number)
+                }
+
+            }
+            .navigationTitle("TimeWizardVP")
+        }
     }
 }
 
